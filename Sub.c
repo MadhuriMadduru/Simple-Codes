fb414593ac5946337fdf18f3a658a488a16d4781
MADHURI MADDURU <madduru.madhuri2006@gmail.com>
2025-09-10 21:08:18 +0530

    Add files via upload

---
+++ Sub.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b ;
+    printf("Enter the First number  :",a);
+    scanf("%d",&a);
+    printf("Enter the Second number  :",b);
+    scanf("%d", &b);
+    printf("The sub : %d ", a -b);
+    return 0;
+}
