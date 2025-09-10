37cb39bb5b30df89ca3ea643d835fe0968382758
MADHURI MADDURU <madduru.madhuri2006@gmail.com>
2025-09-10 21:21:29 +0530

    Add files via upload

---
+++ Sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b ;
+    printf("Enter the First number  :",a);
+    scanf("%d",&a);
+    printf("Enter the Second number  :",b);
+    scanf("%d", &b);
+    printf("The sum : %d ", a +b);
+    return 0;
+}
