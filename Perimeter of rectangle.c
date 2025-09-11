6a2aab2272dd69f53124131ecd71f625aeba28b2
MADHURI MADDURU <madduru.madhuri2006@gmail.com>
2025-09-10 19:49:12 -0700

    Delete Perimeter of rectangle.c

--- Perimeter of rectangle.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main() {
-    float length, width;
-    printf("Enter length and width: ");
-    scanf("%f %f", &length, &width);
-    printf("Perimeter = %.2f\n", 2 * (length + width));
-    return 0;
-}
